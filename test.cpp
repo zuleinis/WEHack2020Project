8f2497f535c5d1ccd051edce3a8dfd4754a312e3
Anna <aquafish2611@gmail.com>
2020-10-10 22:33:42 +0000

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double numOne;
+    cout << "Hi, enter a number!";
+    cin >> numOne;
+
+    cout << "You entered " << numOne;
+
+    return 0;
+}
